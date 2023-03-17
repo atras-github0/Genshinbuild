869be6cbbf38c0e69d27e976420e7c3ae63aac9d
atras <atrasgame75@gmail.com>
2023-03-17 14:34:01 +0900

    l

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.writeFile("test.json", '{"test" : "testdayo"}', (err) => {
+fs.writeFile("test.txt", '{"test" : "testdayo"}', (err) => {
     if (err) rej(err);
     if (!err) {
       console.log('JSONファイルを生成しました');
