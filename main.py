1b5b0f14f95a98d1948096cb8bbc75998a05f43b
atras-github0 <122355709+atras-github0@users.noreply.github.com>
2023-04-02 19:16:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -110,7 +110,6 @@ def handle_postback(event):
 
         computer_path2= f"{cwd}/ImagePr.png"
         dropbox_path2=f"/Images/ImagePr{event.source.user_id}.jpg"
-　　　　　
         for entry in dbx.files_list_folder('/Images').entries:
             print(entry.name)
             if entry == f"Image{event.source.user_id}.jpg":
